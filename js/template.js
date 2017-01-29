defb195fdde4dd72d71591ad69dd0d6bdb669791
Doefamily <itsochi@gmail.com>
2017-01-29 17:38:41 +0300

    g

--- js/template.js
+++ js/template.js
@@ -4,24 +4,21 @@ TrelloPowerUp.initialize({
   'board-buttons': function(t, options){
     return [{
     icon: './images/icon-white.svg',
-    text: 'My Button',
+    text: 'Timer button',
     callback: function(t){
       // do something when the button is clicked
     }}];
   },
-    'board-buttons': function(t, options){
-    return [{
-    icon: './images/icon-white.svg',
-    text: 'My Button2',
+  'card-buttons': function(t, options){
+  return [{
+    icon: './images/icon.svg',
+    text: 'Button Text',
     callback: function(t){
-      // do something when the button is clicked
-    }}];
-  },
-//   'show-settings': function(t, options){
-//     return t.popup({
-//       title: 'Settings',
-//       url: './settings.html',
-//       height: 184
-//     });
-//   }
+      return t.popup({
+        title: "Card Button Popup",
+        url: './card-button-popup.html'
+      });
+    }
+  }];
+}
 });

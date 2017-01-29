47242e57135c517a99cd823903c64b928ee8d9b8
Doefamily <itsochi@gmail.com>
2017-01-29 17:33:58 +0300

    4

--- js/template.js
+++ js/template.js
@@ -1,1 +1,19 @@
-console.log(321);
+console.log('tempalte.js loaded');
+
+TrelloPowerUp.initialize({
+  'board-buttons': function(t, options){
+    return [{
+    icon: './images/icon-white.svg',
+    text: 'My Button',
+    callback: function(t){
+      // do something when the button is clicked
+    }}];
+  },
+//   'show-settings': function(t, options){
+//     return t.popup({
+//       title: 'Settings',
+//       url: './settings.html',
+//       height: 184
+//     });
+//   }
+});
